b5dbe2b946d24da2d4b3e5a2fb13a1e2f035c844
Giulia Lobo <42353909+Giuulob89@users.noreply.github.com>
2022-11-20 12:20:30 -0300

    Finalização da inserção das funções da biblioteca chess.js

--- chessGraph/src/App.jsx
+++ chessGraph/src/App.jsx
@@ -4,7 +4,13 @@ import { Chess } from 'chess.js';
 import { Chessboard } from 'react-chessboard';
 
 export default function App() {
+  const chessboardRef = useRef();
   const [game, setGame] = useState(new Chess());
+
+
+  const [rightClickedSquares, setRightClickedSquares] = useState({});
+  const [moveSquares, setMoveSquares] = useState({});
+  const [optionSquares, setOptionSquares] = useState({});
 
   function safeGameMutate(modify) {
     setGame((g) => {
@@ -12,6 +18,33 @@ export default function App() {
       modify(update);
       return update;
     });
+  }
+
+  //Mostra as opções disponíveis de movimento para o quadrado selecionado
+  function getMoveOptions(square) {
+    const moves = game.moves({
+      square,
+      verbose: true
+    });
+    if (moves.length === 0) {
+      return;
+    }
+
+    const newSquares = {};
+    moves.map((move) => {
+      newSquares[move.to] = {
+        background:
+          game.get(move.to) && game.get(move.to).color !== game.get(square).color
+            ? 'radial-gradient(circle, rgba(0,0,0,.1) 85%, transparent 85%)'
+            : 'radial-gradient(circle, rgba(0,0,0,.1) 25%, transparent 25%)',
+        borderRadius: '50%'
+      };
+      return move;
+    });
+    newSquares[square] = {
+      background: 'rgba(255, 255, 0, 0.4)'
+    };
+    setOptionSquares(newSquares);
   }
 
   //Função que movimenta uma peça no tabuleiro
@@ -33,6 +66,53 @@ export default function App() {
     makeAMove(possibleMoves[randomIndex]);
   }
 
+  //Função que dispara em um clique em um quadrado
+  function onSquareClick(square) {
+    setRightClickedSquares({});
+
+    function resetFirstMove(square) {
+      setMoveFrom(square);
+      getMoveOptions(square);
+    }
+
+    // from square
+    if (!moveFrom) {
+      resetFirstMove(square);
+      return;
+    }
+
+    // attempt to make move
+    const gameCopy = { ...game };
+    const move = gameCopy.move({
+      from: moveFrom,
+      to: square,
+      promotion: 'q' // always promote to a queen for example simplicity
+    });
+    setGame(gameCopy);
+
+    // if invalid, setMoveFrom and getMoveOptions
+    if (move === null) {
+      resetFirstMove(square);
+      return;
+    }
+
+    setTimeout(makeRandomMove, 300);
+    setMoveFrom('');
+    setOptionSquares({});
+  }
+
+  //Função que dispara quando clicar com o botão direito em um quadrado
+  function onSquareRightClick(square) {
+    const colour = 'rgba(0, 0, 255, 0.4)';
+    setRightClickedSquares({
+      ...rightClickedSquares,
+      [square]:
+        rightClickedSquares[square] && rightClickedSquares[square].backgroundColor === colour
+          ? undefined
+          : { backgroundColor: colour }
+    });
+  }
+
   //Funçao que dispara sempre que o jogador clicar em uma peça
   // function onClick()
 
@@ -41,7 +121,49 @@ export default function App() {
 
   return (
     <div className="App">
-      <Chessboard />
+      <Chessboard
+        id="ClickToMove"
+        animationDuration={200}
+        arePiecesDraggable={false}
+        boardWidth={boardWidth}
+        position={game.fen()}
+        onSquareClick={onSquareClick}
+        onSquareRightClick={onSquareRightClick}
+        customBoardStyle={{
+          borderRadius: '4px',
+          boxShadow: '0 5px 15px rgba(0, 0, 0, 0.5)'
+        }}
+        customSquareStyles={{
+          ...moveSquares,
+          ...optionSquares,
+          ...rightClickedSquares
+        }}
+        ref={chessboardRef} />
+      <button
+        className="rc-button"
+        onClick={() => {
+          safeGameMutate((game) => {
+            game.reset();
+          });
+          chessboardRef.current.clearPremoves();
+          setMoveSquares({});
+          setRightClickedSquares({});
+        }}
+      >
+        reset
+      </button>
+      <button
+        className="rc-button"
+        onClick={() => {
+          safeGameMutate((game) => {
+            game.undo();
+          });
+          chessboardRef.current.clearPremoves();
+          setMoveSquares({});
+        }}
+      >
+        undo
+      </button>
     </div>
   )
 }
